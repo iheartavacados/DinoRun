ad969f249f5b90f93ad7d2d19bc7c6e58eba9ba0
dilarakal1 <48969671+dilarakal1@users.noreply.github.com>
2020-02-05 16:47:25 -0800

    Added stuff

--- DinoRunRemake/Assets/Scripts/PointSystem.cs
+++ DinoRunRemake/Assets/Scripts/PointSystem.cs
@@ -7,13 +7,12 @@ public class PointSystem : MonoBehaviour
     float elapsedTime;
     int points;
     int high;
-    public int speedUpFactor = 5;
+    public int speedUpFactor = 2;
     private static TextMesh PointText;
 
     // Start is called before the first frame update
     void Start()
     {
-        points = 0;
         PointText = GameObject.Find("Points").GetComponent<TextMesh>();
     }
 
@@ -22,19 +21,21 @@ public class PointSystem : MonoBehaviour
     {
         if (PlayerController.frozen)
         {
+            //if game pauses, reset regular score
+            //update text to keep highest score
             return;
         }
         elapsedTime += Time.deltaTime * 10;
         points = (int)elapsedTime;
         
-        if (points > high)
-        {
-            high = points;
-        }
+        //if (points > high)
+        //{
+        //    high = points;
+        //}
 
         PointText.text = $"High: {high:00000}   Score: {points:00000}";
 
-        if(points%200 == 0 && points != 0)
+        if(points%100 == 0 && points != 0)
         {
             FlashScore();
             SpeedUp();
@@ -49,5 +50,11 @@ public class PointSystem : MonoBehaviour
     void SpeedUp()
     {
         ObstacleMovement.speed += speedUpFactor;
+    }
+
+    void ResetScore()
+    {
+        
+        
     }
 }
