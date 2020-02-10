10d7ffd951224c7b74632bc7a5b2dcf5495dc37c
Remmie <emily@isimar.com>
2020-02-09 17:54:22 -0800

    Tried to fix background code

--- DinoRunRemake/Assets/Scripts/BackgroundMovement.cs
+++ DinoRunRemake/Assets/Scripts/BackgroundMovement.cs
@@ -11,7 +11,7 @@ public class BackgroundMovement : MonoBehaviour
     static GameObject bg2;
     static Vector3 bg1start = new Vector3();
     static Vector3 bg2start = new Vector3();
-    private static bool delayTrigger;
+    private static int delayTrigger;
 
     void Start()
     {
@@ -31,6 +31,8 @@ public class BackgroundMovement : MonoBehaviour
 
     void Update()
     {
+        if (delayTrigger > 0)
+            delayTrigger--;
         if (PlayerController.frozen)
         {
             rigidbody2d.velocity = new Vector2(0, 0);
@@ -38,12 +40,11 @@ public class BackgroundMovement : MonoBehaviour
         }
         //transform.position = new Vector3(transform.position.x + (ObstacleMovement.speed * Time.deltaTime / ObstacleMovement.speedFactor) * -1, transform.position.y, transform.position.z);
         rigidbody2d.velocity = new Vector2((ObstacleMovement.speed / ObstacleMovement.speedFactor) * -1, rigidbody2d.velocity.y);
-        delayTrigger = false;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject != null && collision.gameObject.name == "Kill Point" && !PlayerController.frozen && !delayTrigger)
+        if (collision.gameObject != null && collision.gameObject.name == "Kill Point" && !PlayerController.frozen && delayTrigger == 0)
         {
             transform.position = new Vector3(transform.position.x + width * 2 - 0.1f, transform.position.y, transform.position.z);
         }
@@ -52,7 +53,7 @@ public class BackgroundMovement : MonoBehaviour
     public static void resetBack()
     {
         print("background reset");
-        delayTrigger = true;  // If resetting background triggers on exit, ignore it.
+        delayTrigger = 3;  // If resetting background triggers on exit, ignore it for first 3 update loops...
         bg1.transform.position = bg1start;
         bg2.transform.position = bg2start;
     }
